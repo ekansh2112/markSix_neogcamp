4abf9df92d282818f83a213f7231876167121058
Ekansh Gupta <ekanshgupta98@gmail.com>
2021-06-28 02:13:06 +0530

    code refactor

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ var api = "https://api.funtranslations.com/translate/minion.json";
 var inputValue = document.querySelector("#myinput");
 var button = document.querySelector(".translate");
 var output = document.querySelector("#outputarea");
+var count = 0;
 button.addEventListener("click", function () {
-	fetch(`${api}?text=${inputValue.value}`)
-		.then((res) => {
-			return res.json();
-		})
-		.then((data) => {
-			output.innerHTML = data.contents.translated;
-		});
+	if (count <= 5) {
+		fetch(`${api}?text=${inputValue.value}`)
+			.then((res) => {
+				if (res.ok) {
+					count++;
+				}
+				return res.json();
+			})
+			.then((data) => {
+				output.innerHTML = data.contents.translated;
+			});
+	} else {
+		alert("You can only talk in banana language 5 times an hour!");
+	}
 });
